3dd7461f3c4c6636c04be7312892a496186b890b
olmj <leemj010504@naver.com>
2022-07-03 01:54:51 +0900

    html

---
+++ main.js
@@ -0,0 +1,15 @@
+let a = 5;
+let b = 2;
+let myName = "minji";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+myName = "leeminji";
+
+console.log("your new name is " + myName);
+
+const dayOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+console.log(dayOfWeek);
